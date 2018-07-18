1e7aaeca8b32c40b814cd23a61a67ca8f05c9455
Tracy B Porter <tbporter@gmail.com>
2018-07-18 10:20:35 -0500

    tracy - added unit tests for clarity. failing blackbox test for list of integers.

--- src/blackboxtest/groovy/com/phg/codingexercises/DivisorsBboxSpec.groovy
+++ src/blackboxtest/groovy/com/phg/codingexercises/DivisorsBboxSpec.groovy
@@ -7,7 +7,7 @@ import spock.lang.Specification
 
 class DivisorsBboxSpec extends Specification {
 
-  def 'basic divisor endpoint returns prime factors'() {
+  def 'basic factor endpoint returns prime factors for single integer'() {
     given:
     RESTClient restClient = new RESTClient('http://localhost:8080')
 
@@ -25,6 +25,28 @@ class DivisorsBboxSpec extends Specification {
     results.size() == 1
     Map factorsFor12 = results.find { it.input == 12 }
     factorsFor12.prime == [2, 2, 3]
-//    factorsFor12.all == [1, 2, 3, 4, 6, 12] as int[]
+  }
+
+  def 'factor endpoint returns prime factors for a list'() {
+    given:
+    RESTClient restClient = new RESTClient('http://localhost:8080')
+
+    when:
+    HttpResponseDecorator response = restClient.get(
+            path: '/api/v1/factors/12,20',
+            headers: ['Accept': 'application/json']
+    )
+
+    then:
+    response.status == 200
+    response.headers.'Content-Type' == 'application/json;charset=UTF-8'
+    response.data.size() == 1
+    List results = response.data.'results'
+    results.size() == 2
+    Map factorsFor12 = results.find { it.input == 12 }
+    factorsFor12.prime == [2, 2, 3]
+
+    Map factorsFor20 = results.find { it.input == 20 }
+    factorsFor20.prime == [2, 2, 5]
   }
 }
