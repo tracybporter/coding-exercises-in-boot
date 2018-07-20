c713b8f5944302347ef8bf98d37e10768118d245
Tracy B Porter <tbporter@gmail.com>
2018-07-20 13:41:50 -0500

    tracy - failing bbox test for a very simple java controller

--- src/blackboxtest/groovy/com/phg/codingexercises/DefaultBootBboxSpec.groovy
+++ src/blackboxtest/groovy/com/phg/codingexercises/DefaultBootBboxSpec.groovy
@@ -5,10 +5,14 @@ import groovyx.net.http.RESTClient
 import spock.lang.Specification
 
 class DefaultBootBboxSpec extends Specification {
-  def 'actuator endpoint returns standard boot links'() {
-    given:
-    RESTClient restClient = new RESTClient('http://localhost:8080')
+  RESTClient restClient
 
+  void setup() {
+    restClient = new RESTClient('http://localhost:8080')
+    restClient.handler.failure = restClient.handler.success
+  }
+
+  def 'actuator endpoint returns standard boot links'() {
     when:
     HttpResponseDecorator response = restClient.get(
             path: '/actuator',
@@ -28,5 +32,19 @@ class DefaultBootBboxSpec extends Specification {
       assert v.href instanceof String
       assert v.templated instanceof Boolean
     }
+  }
+
+  def 'confirm java controller works'() {
+    when:
+    HttpResponseDecorator response = restClient.get(
+            path: '/javaController/health',
+            headers: ['Accept': 'application/json']
+    )
+
+    then:
+    response.status == 200
+    response.headers.'Content-Type' == 'application/json;charset=UTF-8'
+    response.data.size() == 1
+    response.data.message == 'Java is being compiled too.'
   }
 }
