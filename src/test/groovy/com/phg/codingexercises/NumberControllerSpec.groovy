e51fd3c520acc3f324b8cbee875cf72d577cbf74
Tracy B. Porter <tbporter@gmail.com>
2018-07-23 09:27:44 -0500

    Wire in java (#7)
    * tracy - failing bbox test for a very simple java controller
    
    * tracy - java compilation for free i guess.

---
+++ src/test/groovy/com/phg/codingexercises/NumberControllerSpec.groovy
@@ -0,0 +1,27 @@
+package com.phg.codingexercises
+
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.http.HttpStatus.OK
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
+
+class NumberControllerSpec extends Specification {
+  NumberController controller
+  MockMvc mockMvc
+
+  def setup() {
+    controller = new NumberController()
+    mockMvc = standaloneSetup(controller).build()
+  }
+
+  void 'returns a message if everything compiles and runs as expected for a java class'() {
+    when:
+    def response = mockMvc.perform(get("/javaController/health")).andReturn().response
+
+    then:
+    response.status == OK.value
+    response.getContentAsString() == '{"message":"Java is being compiled too."}'
+  }
+}
