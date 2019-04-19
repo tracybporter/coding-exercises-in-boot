8070304c6524c8eb13c6bd3d9a093e809c469794
tracybporter <tbporter@gmail.com>
2019-04-18 20:45:40 -0500

    tracy - just cuz

--- src/main/groovy/com/phg/codingexercises/FactorialCalculator.groovy
+++ src/main/groovy/com/phg/codingexercises/FactorialCalculator.groovy
@@ -9,4 +9,29 @@ class FactorialCalculator {
         (2..input).each { step -> total = total * step }
         return total
     }
+
+    String generateFizzBuzz(int inputValue) {
+        if (inputValue < 2) {
+            return '1'
+        }
+
+        String result = '1'
+        (2..inputValue).each { value ->
+            String appender = ''
+            if (value % 3 == 0) {
+                appender = 'Fizz'
+            }
+
+            if (value % 5 == 0) {
+                appender += 'Buzz'
+            }
+
+            if (appender) {
+                result += appender
+            } else {
+                result += value
+            }
+        }
+        result
+    }
 }
--- src/test/groovy/com/phg/codingexercises/FactorialCalculatorSpec.groovy
+++ src/test/groovy/com/phg/codingexercises/FactorialCalculatorSpec.groovy
@@ -1,13 +1,12 @@
 package com.phg.codingexercises
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class FactorialCalculatorSpec extends Specification {
+    FactorialCalculator calculator = new FactorialCalculator()
 
     void "calculates factorial as product of all subsequent numbers"() {
-        given:
-        FactorialCalculator calculator = new FactorialCalculator()
-
         expect:
         result == calculator.computeFactorial(value)
 
@@ -18,5 +17,20 @@ class FactorialCalculatorSpec extends Specification {
         3     | 6
         25    | 15511210043330985984000000
 
+    }
+
+    @Unroll
+    void 'returns Fizz for multiples of 3 and Buzz for multiples of 5'() {
+        expect:
+        result == calculator.generateFizzBuzz(value)
+
+        where:
+        value | result
+        1     | '1'
+        2     | '12'
+        3     | '12Fizz'
+        4     | '12Fizz4'
+        5     | '12Fizz4Buzz'
+        16    | '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz16'
     }
 }
